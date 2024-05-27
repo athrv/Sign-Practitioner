7b24b558ad96e48007f223943cf31cfe20689fcf
athrv <atapkir274@gmail.com>
2024-05-27 23:56:01 +0530

    sign practitioner/veriffication

---
+++ main.py
@@ -0,0 +1,188 @@
+import pickle
+import os.path
+import PIL.ImageDraw2
+import numpy as np
+import PIL
+import PIL.Image, PIL.ImageDraw
+import cv2 as cv
+import tkinter.messagebox
+from tkinter import *
+from tkinter import simpledialog
+from sklearn.svm import LinearSVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+class DrawingClassifier:
+    def __init__(self):
+        self.class1, class2, class3=None, None, None
+        self.class1_counter, self.class2_counter, self.class3_counter=None, None, None
+        self.clf=None
+        self.proj_name=None
+        self.root=None
+        self.image1=None
+        
+        self.status_label=None
+        self.canvas=None
+        self.draw=None
+        
+        self.brush_width=15
+        
+        self.classes_prompt()
+        self.init_gui()
+        
+    def classes_prompt(self):
+        msg=Tk()
+        msg.withdraw()
+        
+        self.proj_name=simpledialog.askstring("Project Name", "Please enter your project name below", parent=msg)
+        if os.path.exists(self.proj_name):
+            with open(f"{self.proj_name}/{self.proj_name}_data.pickle", "rb") as f:
+                data=pickle.load(f)
+            self.class1=data['c1']
+            self.class2=data['c2']
+            self.class3=data['c3']
+            self.class1_counter=data['c1c']
+            self.class2_counter=data['c2c']
+            self.class3_counter=data['c3c']
+            self.clf=data['clf']
+            self.proj_name=data['pname']
+        else:
+            self.class1=simpledialog.askstring("Class 1", "What is the first class called?", parent=msg)
+            self.class2=simpledialog.askstring("Class 2", "What is the second class called?", parent=msg)
+            self.class3=simpledialog.askstring("Class 3", "What is the third class called?", parent=msg)
+            
+            self.class1_counter=1
+            self.class2_counter=1
+            self.class3_counter=1
+        
+            self.clf=LinearSVC()
+            
+            os.chdir("Sign detection")
+            os.mkdir(self.proj_name)
+            os.chdir(self.proj_name)
+            os.mkdir(self.class1)
+            os.mkdir(self.class2)
+            os.mkdir(self.class3)
+            os.chdir("..")
+        
+    def init_gui(self):
+        WIDTH=500
+        HEIGHT=500
+        WHITE=(255, 255, 255)
+        
+        self.root=Tk()
+        self.root.title(f"NeuralNine Drawing Classifier - {self.proj_name}")
+        self.canvas=Canvas(self.root, width=WIDTH-10, height=HEIGHT-10, bg="white")
+        self.canvas.pack(expand=YES, fill=BOTH)
+        self.canvas.bind("<B1-Motion>", self.paint)
+        
+        self.image1=PIL.Image.new("RGB", (WIDTH, HEIGHT), WHITE)
+        self.draw=PIL.ImageDraw.Draw(self.image1)
+        
+        btn_frame=tkinter.Frame(self.root)
+        btn_frame.pack(fill=X, side=BOTTOM)
+        btn_frame.columnconfigure(0, weight=1)
+        btn_frame.columnconfigure(1, weight=1)
+        btn_frame.columnconfigure(2, weight=1)
+        
+        class1_btn=Button(btn_frame, text=self.class1, command=lambda: self.save(1))
+        class1_btn.grid(row=0, column=0, sticky=W+E)
+        class2_btn=Button(btn_frame, text=self.class2, command=lambda: self.save(2))
+        class2_btn.grid(row=0, column=1, sticky=W+E)
+        class3_btn=Button(btn_frame, text=self.class3, command=lambda: self.save(3))
+        class3_btn.grid(row=0, column=2, sticky=W+E)
+        
+        bm_btn=Button(btn_frame, text="Brush-", command=self.brushminus)
+        bm_btn.grid(row=1, column=0, sticky=W+E)
+        bp_btn=Button(btn_frame, text="Brush+", command=self.brushplus)
+        bp_btn.grid(row=1, column=2, sticky=W+E)
+        
+        clear_btn=Button(btn_frame, text="Clear", command=self.clear)
+        clear_btn.grid(row=1, column=1, sticky=W+E)
+        
+        train_btn=Button(btn_frame, text="Train Model", command=self.train_model)
+        train_btn.grid(row=2, column=0, sticky=W+E)
+        
+        save_btn=Button(btn_frame, text="Save Model", command=self.save_model)
+        save_btn.grid(row=2, column=1, sticky=W+E)
+        
+        load_btn=Button(btn_frame, text="Load Model", command=self.load_model)
+        load_btn.grid(row=2, column=2, sticky=W+E)
+        
+        change_btn=Button(btn_frame, text="Change Model", command=self.rotate_model)
+        change_btn.grid(row=3, column=0, sticky=W+E)
+        
+        predict_btn=Button(btn_frame, text="Predict Model", command=self.predict)
+        predict_btn.grid(row=3, column=1, sticky=W+E)
+        
+        save_everything_btn=Button(btn_frame, text="Save Everything", command=self.save_everything)
+        save_everything_btn.grid(row=3, column=2, sticky=W+E)
+        
+        self.status_label=Label(btn_frame, text=f"Current Model: {type(self.clf).__name__}")
+        self.status_label.config(font=("Arial", 10))
+        self.status_label.grid(row=4, column=1, sticky=W+E)
+        
+        self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
+        self.root.attributes("-topmost", True)
+        self.root.mainloop()
+        
+    
+    def paint(self, event):
+        x1, y1=(event.x-1), (event.y-1)
+        x2, y2=(event.x+1), (event.y+1)
+        self.canvas.create_rectangle(x1, y1, x2, y2, fill="black", width=self.brush_width)
+        self.draw.rectangle(x1, y1, x2+self.brush_width, y2+self.brush_width, fill="black", width=self.brush_width) 
+    
+    def save(self, class_num):
+        self.image1.save("temp.png")
+        img=PIL.Image.open("temp.png")
+        img.thumbnail((50, 50), PIL.Image.ANTIALIAS)
+        
+        if class_num==1:
+            img.save(f"{self.proj_name}/{self.class1}/{self.class1_counter}.png", "PNG")
+            self.class1_counter+=1
+        elif class_num==2:
+            img.save(f"{self.proj_name}/{self.class2}/{self.class2_counter}.png", "PNG")
+            self.class2_counter+=1
+        elif class_num==3:
+            img.save(f"{self.proj_name}/{self.class3}/{self.class3_counter}.png", "PNG")
+            self.class3_counter+=1
+        self.clear()
+        
+    
+    def brushminus(self):
+        if self.brush_width>1:
+            self.brush_width-=1
+    
+    def brushplus(self):
+        self.brush_width+=1
+    
+    def clear(self):
+        self.canvas.delete("all")
+        self.draw.rectangle([0, 0, 1000, 1000], fill="white")
+    
+    def train_model(self):
+        pass
+    
+    def predict(self):
+        pass
+    
+    def rotate_model(self):
+        pass
+    
+    def save_model(self):
+        pass
+    
+    def load_model(self):
+        pass
+    
+    def save_everything(self):
+        pass
+    
+    def on_closing(self):
+        pass
+    
+    
+DrawingClassifier()
